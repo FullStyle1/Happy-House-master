c68a5d230663c7a3e4515b5001c19d3003f82a38
FullStyle <drksh2015@gmail.com>
2024-11-04 08:34:37 -0600

    Actualizacion

---
+++ Interaccion.js
@@ -0,0 +1,20 @@
+function irLogin() 
+      {
+          window.location.href = 'LogIn.html'; 
+      }
+      function irInicio(){
+        window.location.href = 'Index.html';  
+      }
+      function irNosotros()
+      {
+        window.location.href = 'Nosotros.html';  
+      }
+      function registro()
+      {
+        window.location.href = 'Registro.html';
+      }
+    
+      function irLobby()
+      {
+        window.location.href = 'Lobby.html';
+      }
